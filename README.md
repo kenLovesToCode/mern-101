1c646159e15c767d2288a28a46cabdcb869184fa
kenLovesToCode <lintuanken@gmail.com>
2022-06-12 02:10:24 +0800

    completed jwt authentication

---
+++ README.md
@@ -0,0 +1,5 @@
+[Installation and Initialization]
+
+- yarn init
+- yarn add express dotenv mongoose colors
+- yarn add -D nodemon
--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,13 +1,13 @@
-const { request } = require("express");
 const asyncHandler = require("express-async-handler");
 
 const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 //@desc   Get goals
 //@route  GET /api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
   res.status(200).json(goals);
 });
 
@@ -22,6 +22,7 @@ const setGoal = asyncHandler(async (req, res) => {
 
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
 
   res.status(200).json(goal);
@@ -37,6 +38,19 @@ const updateGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Goal not found!");
   }
+
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found!");
+  }
+
+  //make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("Unauthorized user!");
+  }
+
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
@@ -52,6 +66,19 @@ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Goal not found!");
   }
+
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found!");
+  }
+
+  //make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("Unauthorized user!");
+  }
+
   await goal.remove();
   res.status(200).json({ id: req.params.id });
 });
